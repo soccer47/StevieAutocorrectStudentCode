36e5dd3a9b8edf1523fa82ae53849aa6b8379492
soccer47 <stevie.halprin@menloschool.org>
2025-03-08 13:36:10 -0800

    commit

--- src/Autocorrect.java
+++ src/Autocorrect.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -33,24 +34,55 @@ public class Autocorrect {
         // Set editLimit equal to the inputted threshold
         editLimit = threshold;
 
-
     }
 
     // Recursive method for deleting letters
-    public String deleteLetter(String word, int editScore) {
-        if (dict.containsKey(word)) {
-            return
+    public ArrayList<String> deleteLetter(String word) {
+        // ArrayList to contain valid altered versions of the original word
+        ArrayList<String> validWords = new ArrayList<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            String newWord = word.substring(0,i) + word.substring(i);
+            if (dict.containsKey(newWord)) {
+                validWords.add(newWord);
+            }
         }
-    }
-
-    // Recursive method for swapping letters
-    public String[] swapLetter(String word, int editScore) {
-
+        // Return the ArrayList of valid altered versions of the original word
+        return validWords;
     }
 
     // Recursive method for adding letters
-    public String[] addLetter(String word, int editScore) {
+    public ArrayList<String> addLetter(String word) {
+        // ArrayList to contain valid altered versions of the original word
+        ArrayList<String> validWords = new ArrayList<>();
+        String newWord;
+        for (int i = 0; i < word.length(); i++) {
+            for (int j = 97; j <= 122; j++) {
+                newWord = word.substring(0, i) + j + word.substring(i);
+                if (dict.containsKey(newWord)) {
+                    validWords.add(newWord);
+                }
+            }
+        }
+        // Return the ArrayList of valid altered versions of the original word
+        return validWords;
+    }
 
+    // Recursive method for swapping letters
+    public ArrayList<String> swapLetter(String word) {
+        // ArrayList to contain valid altered versions of the original word
+        ArrayList<String> validWords = new ArrayList<>();
+        String newWord;
+        for (int i = 0; i < word.length(); i++) {
+            for (int j = 97; j <= 122; j++) {
+                newWord = word.substring(0,i) + j + word.substring(i + 1);
+                if (dict.containsKey(newWord)) {
+                    validWords.add(newWord);
+                }
+            }
+        }
+        // Return the ArrayList of valid altered versions of the original word
+        return validWords;
     }
 
     /**
@@ -60,6 +92,56 @@ public class Autocorrect {
      * to threshold, sorted by edit distnace, then sorted alphabetically.
      */
     public String[] runTest(String typed) {
+        // If the typed word exists in the dictionary, return an empty array
+        if (dict.containsKey(typed)) {
+            return new String[0];
+        }
+
+        // HashMap to contain possible valid versions of the misspelled word
+        // The keys are the words, and the values are the number of edits away from the original word
+        HashMap<String, Integer> posWords = new HashMap<>();
+
+        // ArrayLists to hold the various lists of possible edited versions of the typed word
+        ArrayList<String> delete;
+        ArrayList<String> add;
+        ArrayList<String> swap;
+
+        // Add typed temporarily to posWords
+        posWords.put(typed, 0);
+
+        // While the threshold hasn't been reached run another round of edits to find possible altered versions of typed
+        for (String word : posWords.keySet()) {
+            for (int i = 0; i < editLimit; i++) {
+                // Get the valid words possible from deleting a letter
+                delete = deleteLetter(typed);
+                for (String newWord : delete) {
+                    // Make sure each possible altered word hasn't already been added to the HashMap before adding it
+                    if (!posWords.containsKey(word)) {
+                        posWords.put(newWord, i);
+                    }
+                }
+                // Get the valid words possible from adding a letter
+                add = addLetter(typed);
+                for (String newWord : add) {
+                    // Make sure each possible altered word hasn't already been added to the HashMap before adding it
+                    if (!posWords.containsKey(word)) {
+                        posWords.put(newWord, i);
+                    }
+                }
+                // Get the valid words possible from swapping a letter
+                swap = swapLetter(typed);
+                for (String newWord : swap) {
+                    // Make sure each possible altered word hasn't already been added to the HashMap before adding it
+                    if (!posWords.containsKey(word)) {
+                        posWords.put(newWord, i);
+                    }
+                }
+            }
+        }
+
+        // Remove the original misspelled word from the HashMap
+        posWords.remove(typed);
+
 
         return new String[0];
     }
