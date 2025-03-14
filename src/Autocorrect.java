874f2af8eea17e1280f019dae4a356f7e8d7dacb
soccer47 <stevie.halprin@menloschool.org>
2025-03-14 13:48:41 -0700

    commit

--- src/Autocorrect.java
+++ src/Autocorrect.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.TreeMap;
 
@@ -91,70 +92,36 @@ public class Autocorrect {
     // Returns the minimum number of edits it would take to make one word match the other (using tabulation)
     public static int editDistance(String word1, String word2) {
         // 2D array representing possible paths for substrings
-        int[][] path = new int[word1.length()][word2.length()];
+        int[][] path = new int[word1.length() + 1][word2.length() + 1];
+
+        // Fill the first row with the set edit distance
+        for (int i = 0; i < word2.length() + 1; i++) {
+            path[0][i] = i;
+        }
+        // Then fill the first column with the set edit distances
+        for (int i = 1; i < word1.length() + 1; i++) {
+            path[i][0] = i;
+        }
 
         // Iterate through the substring board bottom-up (tabulation approach)
-        for (int i = 0; i < word1.length(); i++) {
-            for (int j = 0; j < word2.length(); j++) {
-                // If current letters of the words match, set current index to upper-left diagonal index (if possible)
-                if (word1.charAt(i) == word2.charAt(j)) {
-                    // Make sure the upper left diagonal index exists
-                    if (i > 0 && j > 0) {
-                        path[i][j] = path[i - 1][j - 1];
-                    }
-                    // If not, instead set the current index to the existing of the left or up indexes
-                    else {
-                        // If the left index exists, set the current index to the left index
-                        if (j > 0) {
-                            path[i][j] = path[i][j - 1];
-                            // Make sure the current character hasn't already appeared in its given word
-                            if (word2.substring(0, j).contains(word2.charAt(j) + "")) {
-                                // If the current character is a repeat, add 1 to the current index
-                                path[i][j]++;
-                            }
-                        }
-                        // Otherwise if the up index exists, set the current index to the up index
-                        else if (i > 0) {
-                            path[i][j] = path[i - 1][j];
-                            // Make sure the current character hasn't already appeared in its given word
-                            if (word1.substring(0, i).contains(word1.charAt(i) + "")) {
-                                // If the current character is a repeat, add 1 to the current index
-                                path[i][j]++;
-                            }
-                        }
-                        // Otherwise (if both i and j are 0 and the current letters match) set the current index to 0
-                        else {
-                            path[i][j] = 0;
-                        }
-                    }
+        for (int i = 1; i < word1.length() + 1; i++) {
+            for (int j = 1; j < word2.length() + 1; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    // If the current letters match, set current index to upper-left diagonal index
+                    path[i][j] = path[i - 1][j - 1];
                 }
                 // Otherwise if the letters don't match...
                 else {
                     // If the up-left diagonal index exists, set the current index to one more than
                     // the lowest of the left, up, and up-left diagonal indexes
-                    if (i > 0 && j > 0) {
-                        path[i][j] = Math.min(path[i][j - 1], path[i - 1][j]);
-                        path[i][j] = Math.min(path[i][j], path[i - 1][j - 1]) + 1;
-                    }
-                    // Otherwise set the current index to one more than the existing of the left or up indexes
-                    else {
-                        if (j > 0) {
-                            path[i][j] = path[i][j - 1] + 1;
-                        }
-                        else if (i > 0) {
-                            path[i][j] = path[i - 1][j] + 1;
-                        }
-                        // Otherwise (if i and j are 0 and the current letters don't match) set the current index to 1
-                        else {
-                            path[i][j] = 1;
-                        }
-                    }
+                    path[i][j] = Math.min(path[i][j - 1], path[i - 1][j]);
+                    path[i][j] = Math.min(path[i][j], path[i - 1][j - 1]) + 1;
                 }
             }
         }
 
         // Return the length of the substring path requiring the minimum number of edits
-        return path[word1.length() - 1][word2.length() - 1];
+        return path[word1.length()][word2.length()];
     }
 
 
